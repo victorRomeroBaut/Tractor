87b31ad5ac695eed11b28a0818aa586c573c44bc
Victor Romero Bautista <victor.romerobaut@gmail.com>
2026-03-14 16:26:39 -0600

    rm_buildings

--- robocomp/components/husky_control/build/CMakeFiles/ICE_GenericBase_target.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/victor/Tractor/robocomp/components/husky_control/generated/GenericBase.ice"
-  "CMakeFiles/ICE_GenericBase_target"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ICE_GenericBase_target.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
